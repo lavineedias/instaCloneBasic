569a6c50633c0d7bff3ee805060a1e3f1ed4b09e
lavineedias <lavinedias007@alu.ufc.br>
2021-11-21 17:17:36 -0300

    Insta clone

---
+++ src/SideBar.js
@@ -0,0 +1,106 @@
+import React from 'react'
+
+ const SideBar = () => {
+    return (
+        
+    <div>
+        <div className="flex p-2 items-center space-x-3">
+            <div className="flex-grow">
+              RedeSocial
+              </div>
+            <div>
+                <button>
+                <img src="https://img.icons8.com/material-rounded/24/000000/plus-2-math--v1.png"/>
+                </button>
+            </div>
+            <div>
+                <button>
+                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                </svg>
+                </button>
+            </div>
+            <div>
+                <button>
+                <svg className="w-4.5 h-4.5" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                </svg>
+                </button>
+            </div>
+        </div>
+        <div className="flex p-4 space-x-3 border">
+            <div className="text-center text-xs">
+                <div className="border-2 rounded-full border-pink-600">
+                    <img className="w-14 h-14 rounded-full" src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"/>
+                </div>
+                usuário
+            </div>
+
+            <div className="text-center text-xs">
+                <div className="border-2 rounded-full border-pink-600">
+                    <img className="w-14 h-14 rounded-full" src="https://images.unsplash.com/photo-1584308972272-9e4e7685e80f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"/>
+                </div>
+                usuário
+            </div>
+
+            <div className="text-center text-xs">
+                <div className="border-2 rounded-full border-pink-600">
+                    <img className="w-14 h-14 rounded-full" src="https://images.unsplash.com/photo-1595152452543-e5fc28ebc2b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"/>
+                </div>
+                usuário
+            </div>
+
+            <div className="text-center text-xs">
+                <div className="border-2 rounded-full border-pink-600">
+                    <img className="w-14 h-14 rounded-full" src="https://images.unsplash.com/photo-1584997159889-8bb96d0a2217?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"/>
+                </div>
+                usuário
+            </div>
+            <div className="text-center text-xs">
+                <div className="border-2 rounded-full border-pink-600">
+                    <img className="w-14 h-14 rounded-full" src="https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"/>
+                </div>
+                usuário
+            </div>
+        </div>
+
+        <div className="my-5">
+            <div className="flex p-2 items-center space-x-3">
+            <div className="text-center text-xs">
+                <div className="border-2 rounded-full border-pink-600">
+                    <img className="w-4 h-4 rounded-full" src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"/>
+                </div>
+            </div>
+            <div className="text-xs">
+                nome
+            </div>
+        </div>
+        <img src="https://images.unsplash.com/photo-1609977682604-28918280a356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"/>
+        <div className="flex p-2 items-center space-x-3">
+                    <div>
+                        <button>
+                            <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                            </svg>
+                        </button>
+                    </div>
+                <div>
+                <button>
+                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                </svg>
+                </button>
+                </div>
+                <div>
+                <img className="w-5 h-5" src="https://img.icons8.com/material-rounded/24/000000/filled-sent.png"/>
+                </div>
+        </div>
+        <div className="px-3 text-xs">
+            <p>Curtido po joão e outras 1.050 pessoas</p>
+        </div>
+    </div>
+</div>
+    );
+}
+
+export default SideBar;
